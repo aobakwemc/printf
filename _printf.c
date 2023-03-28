455317d8d9fa2c7d3b84379e1db63c0183fbd250
Aobakwe <aobakwemc@gmail.com>
2023-03-28 21:08:52 +0000

    Files committed by aobakwemc

--- _printf.c
+++ _printf.c
@@ -24,9 +24,15 @@ int _printf(const char *format, ...)
 		if (format[i] != '%')
 		{
 			buffer[buff_ind++] = format[i];
+
 			if (buff_ind == BUFF_SIZE)
 				print_buffer(buffer, &buff_ind);
-																								/* write(1, &format[i], 1);*/
+			/* write(1, &format[i], 1);*/
+			if (format[i] == '\n')
+			{
+				print_buffer(buffer, &buff_ind);
+				write(1, "\n", 1);
+			}
 			printed_chars++;
 		}
 		else
@@ -38,9 +44,10 @@ int _printf(const char *format, ...)
 			size = get_size(format, &i);
 			++i;
 			printed = handle_print(format, &i, list, buffer,
-				flags, width, precision, size);
-																								if (printed == -1)
+					flags, width, precision, size);
+			if (printed == -1)
 				return (-1);
+
 			printed_chars += printed;
 		}
 	}
